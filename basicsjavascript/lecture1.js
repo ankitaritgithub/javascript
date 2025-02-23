3beddd9d4b83a12c60e20f56a4c685268954dc06
ANKITA SINGH <122306095+ankitaritgithub@users.noreply.github.com>
2025-02-23 05:12:27 +0000

    firstlecture

---
+++ basicsjavascript/lecture1.js
@@ -0,0 +1,10 @@
+console.log("akita singh")
+const accountId = 14453
+let accountEmail = "12154584"
+var accountPassword = "password13"
+/* const can be changes once it get decalred*/
+// accountId = 1213  rror because it stays that we can'nt changes the const terms
+accoountCity = "data"
+//  use only let dont use var because of with var we dont have the scope block  function and functional scope
+let accountData; 
+console.table([accountId, accountEmail, accountPassword,accoountCity, accountData])
