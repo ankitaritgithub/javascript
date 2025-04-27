352c875dd47c0c052fa01cf16ef33ff7b68c2d3c
ANKITA SINGH <122306095+ankitaritgithub@users.noreply.github.com>
2025-04-27 13:04:30 +0000

    done

--- basicsjavascript/03_conversionOperation.js
+++ basicsjavascript/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn); // true
 
 // 1 => true; 0 => false
 // "" => false
@@ -46,15 +46,15 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3); //
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //1
+// console.log(+""); //0
 
 let num1, num2, num3
 
--- basicsjavascript/04_comparision.js
+++ basicsjavascript/04_comparision.js
@@ -4,21 +4,25 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+// comparison with two different datatype - somtimes it give correct by converting that one datatype as per other value but 
+// somtimes it woulnt give correct answer  so its recomended to not to do that
+// console.log("2" > 1); // true
+// console.log("02" > 1); //true
 
-// console.log("2" > 1);
-// console.log("02" > 1);
 
+
+//comparison convert null to a number traeted it as 0
 console.log(null > 0); // false
 console.log(null == 0); // false
-console.log(null >= 0); // false
+console.log(null >= 0); // true
 
 
 //  the reason is thar eqality operator == and comparison operator work differentlt
 //  comparison convert the null as number and treates as 0 and  that  is why 3 is tru eand 1 and 2 is false
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); // false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
 // ===  <-  check value along with datatype stcklly 
 
