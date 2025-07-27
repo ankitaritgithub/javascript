fc1c836912e3f03e6a7fd6174e965067617929f5
ANKITA SINGH <122306095+ankitaritgithub@users.noreply.github.com>
2025-07-27 13:00:13 +0000

    hhn

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,11 +1,17 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+/**1. if in scenrion where we dont want any kind of population  basically clearn and  clear global scope
+ * 2. and If i want  that after that it get excute after that i
+ *  Basically we are facing the global scope  problem ki solve karnea kea liyea
+ *  and immediate function invoke karnea liyea iife ka use kartea hai 
+ *  */
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
 
-( (name) => {
+
+// how to do with arrow function this ifie function
+( (name) => { // simple iife
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
